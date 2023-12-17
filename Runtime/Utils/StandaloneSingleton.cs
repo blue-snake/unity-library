501a21e158249a2f2a8962e4be9c1e9be160e1fe
ReaperMaga <reapermaga@gmail.com>
2023-12-17 05:25:53 +0100

    Added StandaloneSingleton

---
+++ Runtime/Utils/StandaloneSingleton.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlueSnake.Utils {
+    public class StandaloneSingleton<T> where T : StandaloneSingleton<T> {
+        protected static T instance;
+
+        public virtual void OnCreate() { }
+
+        public static T GetInstance() {
+            return instance ??= Activator.CreateInstance<T>();
+        }
+    }
+}
