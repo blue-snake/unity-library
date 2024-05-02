9c55d07795cd744e7a30cca7c77570773fad9fab
ReaperMaga <reapermaga@gmail.com>
2024-05-02 22:14:24 +0200

    Reverted

--- Runtime/UI/Menu/MenuManager.cs
+++ Runtime/UI/Menu/MenuManager.cs
@@ -43,11 +43,6 @@ namespace BlueSnake.UI.Menu {
                 return null;
             }
             Menu menu = Menus[id];
-            if (IsAnyMenuOpen()) {
-                if (!menu.HasParent()) {
-                    return null;
-                }
-            }
             if (menu.HasParent()) {
                 if (!IsMenuOpen(menu.parent.id)) {
                     return null;
